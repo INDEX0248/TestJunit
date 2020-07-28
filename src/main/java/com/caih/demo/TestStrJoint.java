aa7ceab8193ab276172ac8408a587aa2424618d6
huangdong <huangjy_index@163.com>
2020-07-28 14:53:21 +0800

    完全提交

---
+++ src/main/java/com/caih/demo/TestStrJoint.java
@@ -0,0 +1,13 @@
+package com.caih.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestStrJoint {
+    @Test
+    public void test(){
+        String str = StrUtil.strJoint("user_create_time");
+        assertEquals(str, "userCreateTime");
+    }
+}
